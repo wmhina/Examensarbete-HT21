f310f84f669fa5bb7e9c7e12e19bd93c9db5a7d6
Codescode <Alexander.dahlen.student@hotmail.com>
2021-12-02 18:00:49 +0100

    //En start till en databas, barebones och mycket att diskutera om vad som hör hemma här

---
+++ src/main/java/com/deque/html/axecore/resources/Database.java
@@ -0,0 +1,49 @@
+package com.deque.html.axecore.resources;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public final class Database {
+    private static Database instance;
+    private final List<String> webpages = new ArrayList<>();
+
+    private Database() throws MalformedURLException {
+        loadData();
+    }
+
+    public static Database getInstance() throws MalformedURLException {
+        if (instance == null) instance = new Database();
+        return instance;
+    }
+
+    private void add(String webpage) throws MalformedURLException {
+        webpages.add(webpage);
+    }
+
+    public Collection<String> getWebpages() {
+        return Collections.unmodifiableCollection(webpages);
+    }
+
+    private void loadData() throws MalformedURLException {
+        add("https://www.arn.se/");
+        add("https://adokweb.arn.se/digiforms2/htmlViewer?xsessiontag=1403809502");
+        add("http://www.vetansvar.se/");
+        add("http://www.vetansvar.se/gorenanmalan.4.7eee19da168944000cb8ae5e.html");
+        add("https://arbetsformedlingen.se/");
+        //TODO: Ska arbetsförmedlingen ha en, flera eller inga extra sidor testade?
+        add("https://www.arbetsgivarverket.se/");
+        add("https://www.av.se/");
+        //TODO: Hur många funktioner ska vi ta med? Det blir många även i två steg från hemsidan
+        add("https://www.arvsfonden.se/");
+        //TODO:Testa idé med som sida att testas?
+        add("https://www.barnombudsmannen.se/");
+        add("https://www.bth.se/");
+        //Todo: hur mycket ska tas med här?
+        add("https://www.bfn.se/");
+        add("https://www.bolagsverket.se/");
+        add("https://www.boverket.se/");
+    }
+}
